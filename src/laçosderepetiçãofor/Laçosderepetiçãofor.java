d9918b9a427001bdbf26162440003c14f6d6a1ea
LizandraSantos0 <lizandra.santos1710@gmail.com>
2023-09-04 11:22:14 -0300

    Primeiro comentario

--- src/laçosderepetiçãofor/Laçosderepetiçãofor.java
+++ src/laçosderepetiçãofor/Laçosderepetiçãofor.java
@@ -1,18 +1,18 @@
 package laçosderepetiçãofor;
-
+import java.util.Scanner;
 public class Laçosderepetiçãofor {
 
 	public static void main(String[] args) {
-	Scanner leia = new Scanner();
+	Scanner leia = new Scanner( System.in);
 	String number;
 	System.out.println("Digite o dois números inteiros onde o primeiro deve ser menor que o segundo: "); 
-	int num1 = leia.nextlnt();
-	int num2 = leia.nextlnt();
+	int num1 = leia.nextInt();
+	int num2 = leia.nextInt();
 	if ( num1 >= num2 ); {
 	System.out.println("intervalo invalido");
 	System.exit(0);
 	}
-	for ( int i = num1; i <= num2;  i++ ) {
+	for ( int i = num1; i <= num2;  i ++ ) {
 		if ( i  % 3 ==0 && i  %  5 ==0)
 			
 			System.out.println(i);
